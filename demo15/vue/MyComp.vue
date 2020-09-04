e35a8fc79445db00807cff64fe8d38928b3f508e
lizhigao <lizhigao@021.com>
2020-09-04 18:31:51 +0800

    vue serve

---
+++ demo15/vue/MyComp.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="component">
+    <h3>自定义comp组件</h3>
+    <p>初始值：{{ v }}</p>
+    <button @click="minus">减1</button>
+    <button @click="add">加1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // event 自定义为 change，也可以不声明model，直接用默认的input事件this.$emit('input', value);
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+  props: {
+    value: {
+      type: Number,
+      default: 0,
+    },
+  },
+  mounted() {
+    console.log('[value]', this.value);
+  },
+  computed: {
+    v() {
+      return this.value;
+    },
+  },
+  methods: {
+    minus() {
+      this.$emit('change', this.value - 1);
+    },
+    add() {
+      this.$emit('change', this.value + 1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.component {
+  padding: 15px;
+  border: 2px dashed yellowgreen;
+  background-color: lemonchiffon;
+}
+</style>
---
+++ demo15/vue/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="root">
+    <h3>v-model在非表单input组件应用</h3>
+    <my-comp v-model="compValue1"></my-comp>
+    <p>父组件：{{ compValue1 }}</p>
+    <br />
+    <my-comp v-model="compValue2"></my-comp>
+    <p>父组件：{{ compValue2 }}</p>
+  </div>
+</template>
+
+<script>
+import MyComp from './MyComp';
+export default {
+  components: {
+    MyComp,
+  },
+  data() {
+    return {
+      compValue1: 10,
+      compValue2: 0,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
